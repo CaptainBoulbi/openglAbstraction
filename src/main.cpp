6b54e59da87c199f93254afebb97a515d5013309
_N3m0 <n3m0o7@hotmail.com>
2023-09-25 07:50:19 +0200

    info aligné

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 
 void info(){
 	std::cout << "\033[2J\033[7A";
-	std::cout << "Life Time :";
+	std::cout << "Life Time :\t";
 	std::cout << currentTime << std::endl;
-	std::cout << "FPS :";
+	std::cout << "FPS :\t\t";
 	std::cout << 1 / deltaTime << std::endl;
-	std::cout << "average FPS :";
+	std::cout << "average FPS :\t";
 	std::cout << nbFrame / currentTime << std::endl;
-	std::cout << "MS :";
-	std::cout << deltaTime << std::endl;
+	std::cout << "MS :\t\t";
+	std::cout << deltaTime * 1000 << std::endl;
 }
 
 int main(){
