525464ed80361b5dd86747b43fd2b2297dd68fdd
_N3m0 <n3m0o7@hotmail.com>
2023-09-25 14:35:25 +0200

    clear color beg frame

--- include/engine.hpp
+++ include/engine.hpp
@@ -72,6 +72,8 @@ bool engineShouldClose(){
 }
 
 void engineBeginFrame(){
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	processTime();
 	processInput();
 }
