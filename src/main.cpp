d596446949612b24d823b0a968bbb7e63889e800
_N3m0 <n3m0o7@hotmail.com>
2023-09-25 07:47:46 +0200

    info

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,24 @@
 
 #include <iostream>
 
+void info(){
+	std::cout << "\033[2J\033[7A";
+	std::cout << "Life Time :";
+	std::cout << currentTime << std::endl;
+	std::cout << "FPS :";
+	std::cout << 1 / deltaTime << std::endl;
+	std::cout << "average FPS :";
+	std::cout << nbFrame / currentTime << std::endl;
+	std::cout << "MS :";
+	std::cout << deltaTime << std::endl;
+}
+
 int main(){
 	engineStartUp("hamood");
 	engineResizeScreen(50, 50);
 
-	std::cout << SCR_WIDTH << ':' << SCR_HEIGHT << std::endl;
-
 	while (!engineShouldClose()){
-		//std::cout << currentTime << ':' << deltaTime << std::endl;
+		info();
 		engineSwapBuffers();
 	}
 
