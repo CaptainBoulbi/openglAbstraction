d9071450b34b9c9d92d1a5ea3662b1bbc14b0161
_N3m0 <n3m0o7@hotmail.com>
2023-09-30 13:15:56 +0200

    commentaire expliquationelle neuronal

--- include/simpleGraphics.hpp
+++ include/simpleGraphics.hpp
@@ -14,8 +14,12 @@ namespace sg{
 
   // GENERAL
 
+  // setup glfw, glad et opengl config
   void setup();
+  // enable / disable vsync
+  // driver config will override it
   void setVsync(bool etat);
+  // console log all opengl error
   void glError();
 
   // WINDOW
@@ -35,10 +39,15 @@ namespace sg{
 
     Window(unsigned int width, unsigned int height, const char* title);
     ~Window();
+    // end life of window
     void destroy();
+    // send a close signal to the window
     void close();
+    // draw everithing to the window
     void display();
+    // return if the window is open or if a close signal is sent
     bool isOpen();
+    // change the position of the window in the screen
     void setPos(unsigned int x, unsigned int y);
     void getPos(unsigned int* x, unsigned int* y);
     //void setPos(vec2 pos);
@@ -47,10 +56,16 @@ namespace sg{
     //void setSize(vec2 size);
     void setTitle(const char* title);
     //void setIcon(unsigned int width, unsigned int height, const Uint8 *pixels)
+    // show / hide the window
     void setVisible(bool etat);
+    // lock the cursor in the window area and make it invisible
     void setCursorLock(bool etat);
+    // send a signal to the os that the window need focus
+    // on ubuntu it send a notification
     void requestFocus();
+    // put the window in the first plan
     void forceFocus();
+    // return if the window has the focus (if the user is on the window)
     bool hasFocus();
     void setFullscreen(bool etat);
 
